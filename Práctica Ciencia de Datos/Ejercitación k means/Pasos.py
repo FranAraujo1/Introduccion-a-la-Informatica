413341d8f6e13cef0a8405eb93f60e3b7a054a77
FranAraujo1 <faraujoar@gmail.com>
2021-06-24 12:07:18 -0300

    commit
    commit

--- Práctica Ciencia de Datos/Ejercitación k means/Pasos.py
+++ Práctica Ciencia de Datos/Ejercitación k means/Pasos.py
@@ -33,18 +33,20 @@ print(stock_data.dropna(inplace=True))
 
 # Más información
 print(stock_data.describe())
-print(stock_data.info())
+print(stock_data.info(max_cols=1000))
 
 # Gráfico
 f = sns.histplot(data = stock_data, x = "2010-01-04", binwidth=0.25, kde = True, color= 'orange')
 print(f)
 #plt.show()
 
+#Nan # duplicados # outlayers
+
 # Rellenamos los 'espacios' de las columnas donde había nulls con la media
 print(stock_data.fillna(stock_data['2010-01-07'].mean(), inplace=True)) 
 print(stock_data.fillna(stock_data['2013-10-23'].mean(), inplace=True))
 
-# Filtramos los valores
+# Filtramos los valores 
 df2 = stock_data.sort_values(by = ["2013-10-29"], ascending = False, ignore_index = True).head(10)
 print(df2)
 
@@ -53,7 +55,21 @@ print(df2)
 
 stock_data_normalizado = 
 
+# cuantiles
+Q1 = stock_data['2010-01-04'].quantile(0.07) # -0.4435993499999751
+Q2 = stock_data['2010-01-04'].quantile(0.93) # 0.9708959100000023
 
+# 
+outliers = df[df[col] > df[col].mean() + 3 * df[col].std()]
+outliers = df[df[col] < df[col].mean() - 3 * df[col].std()]
+
+# la normalización sirve para trabajar mejor con los datos, perdes un poco la identidad de los mismos
+
+scaler = StandardScaler()
+stock_data_normalizado = scaler.fit_transform(stock_data)
+
+# siempre hacer un test - 
+# si es mayor a 0,005 es normal
 
 '''k = 14  #definimos la cantidad de clusters
 kmeans = KMeans(n_clusters = k, init="random", n_init=10, max_iter=300, random_state=123457) #tomamos los centroides de forma aleatoria y definimos un máximo de 300 iteraciones
